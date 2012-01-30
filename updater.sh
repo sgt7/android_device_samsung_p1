8e457e19c2ecd250f7cd63b14531d533e4f7f8fe
Chirayu Desai <chirayudesai1@gmail.com>
2012-01-30 15:04:30 +0530

    moar fixes
    Change-Id: I2a690596edb5996b316eae44e2080c2ab1cdeee3

--- updater.sh
+++ updater.sh
@@ -44,31 +44,32 @@ if /tmp/busybox test -e /dev/block/bml7 ; then
     # make sure sdcard is mounted
     check_mount /mnt/sdcard $SD_PART vfat
 
-    # everything is logged into /mnt/sdcard/cyanogenmod_bml.log
-    set_log /mnt/sdcard/cyanogenmod_bml.log
+    # everything is logged into /sdcard/cyanogenmod_bml.log
+    set_log /sdcard/cyanogenmod_bml.log
 
     if $IS_GSM ; then
         # make sure efs is mounted
-        check_mount /efs /dev/block/stl3 rfs
+        #check_mount /efs /dev/block/stl3 rfs
 
         # create a backup of efs
-        if /tmp/busybox test -e /mnt/sdcard/backup/efs ; then
-            /tmp/busybox mv /mnt/sdcard/backup/efs /mnt/sdcard/backup/efs-$$
+        mount /dev/block/stl3 /efs
+        if /tmp/busybox test -e /sdcard/backup/efs ; then
+            /tmp/busybox mv /sdcard/backup/efs /sdcard/backup/efs-$$
         fi
-        /tmp/busybox rm -rf /mnt/sdcard/backup/efs
+        /tmp/busybox rm -rf /sdcard/backup/efs
 
-        /tmp/busybox mkdir -p /mnt/sdcard/backup/efs
-        /tmp/busybox cp -R /efs/ /mnt/sdcard/backup
+        /tmp/busybox mkdir -p /sdcard/backup/efs
+        /tmp/busybox cp -R /efs/ /sdcard/backup
     fi
 
     # write the package path to sdcard cyanogenmod.cfg
     if /tmp/busybox test -n "$UPDATE_PACKAGE" ; then
         PACKAGE_LOCATION=${UPDATE_PACKAGE#/mnt}
-        /tmp/busybox echo "$PACKAGE_LOCATION" > /mnt/sdcard/cyanogenmod.cfg
+        /tmp/busybox echo "$PACKAGE_LOCATION" > /sdcard/cyanogenmod.cfg
     fi
 
     # Scorch any ROM Manager settings to require the user to reflash recovery
-    /tmp/busybox rm -f /mnt/sdcard/clockworkmod/.settings
+    /tmp/busybox rm -f /sdcard/clockworkmod/.settings
 
     # write new kernel to boot partition
     /tmp/flash_image boot /tmp/boot.img
