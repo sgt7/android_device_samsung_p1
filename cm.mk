f1e8cf6ffbb977d9b136fb0724823d81cfc89b8d
Erich Boehm <eaut@boehm.org>
2012-04-02 22:06:37 +0200

    P1 is a GSM phone
    Change-Id: I183f94110b02f493c5fd8b99d7ceb1fe93cd5117

--- cm.mk
+++ cm.mk
@@ -7,6 +7,9 @@ TARGET_BOOTANIMATION_NAME := vertical-540x960
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# P1 is a GSM phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/samsung/p1/full_p1.mk)
 
