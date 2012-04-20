acd1372e479905f030880b9ed23d3cdddc7b109d
Humberto Borba <humberos@gmail.com>
2012-04-20 01:39:11 -0300

    Move bootanimation reference to p1-common
    Change-Id: I0e870fcd6779c599d110fe29b32811c877fefcad

--- cm.mk
+++ cm.mk
@@ -4,9 +4,6 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := P1
 
-# Bootanimation
-TARGET_BOOTANIMATION_NAME := vertical-540x960
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
