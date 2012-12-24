2946253abfc9881de306a8d7f57f85bcabde0c63
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-24 16:04:08 +0530

    fix bootimage installation for p1l / p1n
    Change-Id: I54fed1b966371abe3e86675b10fd6aa6f1eb43d9

--- p1ln.sh
+++ p1ln.sh
@@ -1,15 +1,28 @@
 HW=$(getprop ro.hardware | tr '[A-Z]' '[a-z]')
-DEVICE=$(getprop ro.product.device | tr '[A-Z]' '[a-z]')
+DEVICE=$(cat /default.prop | grep ro.cm.device | sed s#ro\.cm\.device=## | tr '[A-Z]' '[a-z]')
 
 case $HW in
 p1|gt-p1000)
-model=p1
+	case $DEVICE in
+	p1)
+		model=p1
+	;;
+	p1l)
+		model=p1l
+	;;
+	p1n)
+		model=p1n
+	;;
+	*)
+	echo "Invalid device: $DEVICE"
+	;;
+	esac
 ;;
 p1l|gt-p1000l)
-model=p1l
+	model=p1l
 ;;
 p1n|gt-p1000n)
-model=p1n
+	model=p1n
 ;;
 *)
 echo "Invalid model: $HW"
@@ -17,22 +30,7 @@ exit 2
 ;;
 esac
 
-if [ "$model" == "p1" ]
-case $DEVICE in
-p1|gt-p1000|galaxytab)
-;;
-p1l|gt-p1000l)
-model=p1l
-;;
-p1n|gt-p1000n)
-model=p1n
-;;
-*)
-echo "Invalid device: $DEVICE"
-;;
-esac
-
-if [ "$model" != "p1" ]
+if [ "$model" != "p1" ] ; then
 mv /tmp/boot_$model.img /tmp/boot.img
 fi
 
