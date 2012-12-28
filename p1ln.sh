03a254b4438351cb59b96c8fe119b19a395022d7
Humberto Borba <humberos@gmail.com>
2012-12-28 01:13:14 -0200

    p1ln: fixes
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- p1ln.sh
+++ p1ln.sh
@@ -1,37 +1,38 @@
-HW=$(getprop ro.hardware | tr '[A-Z]' '[a-z]')
-DEVICE=$(cat /default.prop | grep ro.cm.device | sed s#ro\.cm\.device=## | tr '[A-Z]' '[a-z]')
-
+#!/tmp/busybox sh
+#
+# Setting the GSM Kernel Samsung Galaxy Tab 7"
+# (c) 2012 by SGT7 TE4M
+#------------------------------------------------------------------------
+HW=`/tmp/busybox cat /proc/cpuinfo | /tmp/busybox grep Hardware | /tmp/busybox sed s/Hardware// | /tmp/busybox tr '[A-Z]' '[a-z]' | /tmp/busybox tr -d ' ' | /tmp/busybox tr -d ':' | /tmp/busybox tr -d '\t'`
+DEVICE=`/tmp/busybox cat /default.prop | /tmp/busybox grep ro.cm.device | /tmp/busybox sed s#ro\.cm\.device=## | /tmp/busybox tr '[A-Z]' '[a-z]'`
+#------------------------------------------------------------------------
+# Set GT-P1000 as default variant
+#------------------------------------------------------------------------
+model=p1
+#------------------------------------------------------------------------
 case $HW in
-p1|gt-p1000)
-	case $DEVICE in
-	p1)
-		model=p1
-	;;
-	p1l)
+	p1l|gt-p1000l)
 		model=p1l
 	;;
-	p1n)
+	p1n|gt-p1000n)
 		model=p1n
 	;;
-	*)
-	echo "Invalid device: $DEVICE"
-	;;
-	esac
-;;
-p1l|gt-p1000l)
-	model=p1l
-;;
-p1n|gt-p1000n)
-	model=p1n
-;;
-*)
-echo "Invalid model: $HW"
-exit 2
-;;
 esac
-
+#------------------------------------------------------------------------
+if [ "$model" == "p1" ] ; then
+	case $DEVICE in
+		p1l|p1n)
+			model=$DEVICE
+		;;
+		*)
+			/tmp/busybox echo "Invalid device: $DEVICE"
+		;;
+	esac
+fi
+#------------------------------------------------------------------------
 if [ "$model" != "p1" ] ; then
-mv /tmp/boot_$model.img /tmp/boot.img
+	/tmp/busybox rm /tmp/boot.img
+	/tmp/busybox mv /tmp/boot_$model.img /tmp/boot.img
 fi
 
 exit 0
