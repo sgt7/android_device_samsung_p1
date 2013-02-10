c7cf5f0082f9a568b7b71bd950e52d879ceca4c8
Chirayu Desai <cdesai@cyanogenmod.org>
2013-02-10 18:47:29 +0530

    switch to aries kernel
    Change-Id: I518ad2ce9887596a0a9721f492e0b8dc467293ba

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/p1/full_p1.mk)
 
 # kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/p1
+TARGET_KERNEL_SOURCE := kernel/samsung/aries
 TARGET_KERNEL_CONFIG := cyanogenmod_p1_defconfig
 
 PRODUCT_NAME := cm_p1
